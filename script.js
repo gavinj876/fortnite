25f3a9dff948e8a62a793f937e7ee7a9972914aa
Gavin Johnson <gavin.johnson@wccschools.org>
2025-06-04 14:02:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//AI helped me shuffle the questions
+for (let i = questions.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [questions[i], questions[j]] = [questions[j], questions[i]];
+}
+
 let questions = [
     {
         question: "Which Skin Has A Built-In Emote?",
